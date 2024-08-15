a1418bc05b8a3fecd3a3f15a8b8ef2cc283d014c
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2024-08-15 20:01:14 +0100

    exposing stitch_align and stitch_fuse

--- zetastitcher/__init__.py
+++ zetastitcher/__init__.py
@@ -3,3 +3,5 @@ from .version import __version__
 from .io.inputfile import InputFile
 from zetastitcher.align.filematrix import FileMatrix
 from zetastitcher.fuse.virtual_fused_volume import VirtualFusedVolume
+from zetastitcher.fuse.__main__ import stitch_fuse
+from zetastitcher.align.__main__ import stitch_align
