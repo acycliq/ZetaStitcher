1fdfca785e26310becad9a01a3e6f62c85f9003d
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2024-08-15 19:04:54 +0100

    removed a ref to requirements.in, doesnt exist anymore

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open('requirements.in') as f:
-    install_requires = f.read().split()
-
 version = {}
 with open("zetastitcher/version.py") as fp:
     exec(fp.read(), version)
